9a7ccf2bda01b78b09055025e5f1ce4b94734a63
Steve Billings <stevebillings@system76-pc.localdomain>
2022-11-15 16:15:45 -0500

    refactoring

--- src/lidar_monitor/src/subscriber.cpp
+++ src/lidar_monitor/src/subscriber.cpp
@@ -2,6 +2,24 @@
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 
+static const int STATE_SEARCH = 0;
+static const int STATE_HUGGING = 1;
+static const int STATE_RECENTLY_LOST = 2;
+static constexpr float SPEED = 1.0;
+static const int DIR_UNKNOWN = 0;
+static const int DIR_AHEAD = 1;
+static const int DIR_LEFT = 2;
+static const int DIR_RIGHT = 3;
+static const int OBSTACLE_LAST_SEEN_NOWHERE = 0;
+static const int OBSTACLE_LAST_SEEN_LEFT = 1;
+static const int OBSTACLE_LAST_SEEN_RIGHT = 2;
+static const int OK_LIMIT = 5.0;
+static const int NEAR_LIMIT = 3.0;
+static const int DIST_NAME_NEAR = 0;
+static const int DIST_NAME_IDEAL = 1;
+static const int DIST_NAME_FAR = 2;
+static const double SPEED_DIVISOR_FOR_SPIN_YAW = 4.0;
+static const double SPEED_DIVISOR_FOR_CURVE_YAW = 5.0;
 using std::placeholders::_1;
 using namespace std::chrono_literals;
 
@@ -77,7 +95,7 @@ class SubscriberNode : public rclcpp::Node {
         }
 
 
-    bool is_obstacle_within_range() {
+        bool is_obstacle_within_range() {
             return ((ahead_dist_ == DIST_NAME_NEAR || ahead_dist_ == DIST_NAME_IDEAL)
                     || (left_dist_ == DIST_NAME_NEAR || left_dist_ == DIST_NAME_IDEAL)
                     || (right_dist_ == DIST_NAME_NEAR || right_dist_ == DIST_NAME_IDEAL));
@@ -95,20 +113,7 @@ class SubscriberNode : public rclcpp::Node {
                     && (right_dist_ == DIST_NAME_FAR));
         }
 
-        bool is_obstacle_distance_ideal() {
-            bool one_is_ideal = ((ahead_dist_ == DIST_NAME_IDEAL)
-                    || (left_dist_ == DIST_NAME_IDEAL)
-                    || (right_dist_ == DIST_NAME_IDEAL));
-
-            return one_is_ideal && !is_obstacle_too_near();
-        }
-
         void interpret_laser(sensor_msgs::msg::LaserScan::SharedPtr msg) {
-			/*
-            RCLCPP_INFO(get_logger(), "Received LaserScan angle_min: %f; angle_max: %f; angle_increment: %f",
-				msg->angle_min, msg->angle_max,
-				msg->angle_increment);
-			 */
 			int i = 0;
 			auto min_range = 999.999;
 			int min_range_index = -1;
@@ -119,13 +124,11 @@ class SubscriberNode : public rclcpp::Node {
 				}
 				i++;
 			}
-			// RCLCPP_INFO(get_logger(), "\tfound min range %f at index %d", min_range, min_range_index);
             int dead_ahead = msg->ranges.size() / 2;
             int ahead_width = msg->ranges.size() / 10;
             int ahead_width_from_center = ahead_width / 2;
             int right_limit = dead_ahead - ahead_width_from_center;
             int left_limit = dead_ahead + ahead_width_from_center;
-            // RCLCPP_INFO(get_logger(), "Ahead is %d to %d", right_limit, left_limit);
             if (min_range < OK_LIMIT) {
                 if (min_range_index < right_limit) {
                     RCLCPP_INFO(get_logger(), "Obstacle on right: %f meters", min_range);
@@ -206,7 +209,7 @@ class SubscriberNode : public rclcpp::Node {
             drive_message_.linear.z = 0.0;
             drive_message_.angular.x = 0.0;
             drive_message_.angular.y = 0.0;
-            drive_message_.angular.z = -1 * SPEED / 5.0; // yaw
+            drive_message_.angular.z = -1 * SPEED / SPEED_DIVISOR_FOR_CURVE_YAW;
             drive_publisher_->publish(drive_message_);
         }
 
@@ -217,12 +220,12 @@ class SubscriberNode : public rclcpp::Node {
             drive_message_.linear.z = 0.0;
             drive_message_.angular.x = 0.0;
             drive_message_.angular.y = 0.0;
-            drive_message_.angular.z = SPEED / 5.0; // yaw
+            drive_message_.angular.z = SPEED / SPEED_DIVISOR_FOR_CURVE_YAW;
             drive_publisher_->publish(drive_message_);
         }
 
         void spin() {
-            float yaw = SPEED / 4.0; // default to spin left
+            float yaw = SPEED / SPEED_DIVISOR_FOR_SPIN_YAW; // default to spin left
             if (obstacle_last_seen_ != OBSTACLE_LAST_SEEN_NOWHERE) {
                 if ((obstacle_last_seen_ == OBSTACLE_LAST_SEEN_RIGHT) && (right_dist_ == DIST_NAME_FAR)) {
                     yaw = -1.0 * yaw; // switch to spin right
@@ -249,26 +252,11 @@ class SubscriberNode : public rclcpp::Node {
         rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr drive_publisher_;
         geometry_msgs::msg::Twist drive_message_;
         rclcpp::Time state_ts_;
-        static constexpr float SPEED = 1.0;
-        static const int DIR_UNKNOWN = 0;
-        static const int DIR_AHEAD = 1;
-        static const int DIR_LEFT = 2;
-        static const int DIR_RIGHT = 3;
-        static const int OBSTACLE_LAST_SEEN_NOWHERE = 0;
-        static const int OBSTACLE_LAST_SEEN_LEFT = 1;
-        static const int OBSTACLE_LAST_SEEN_RIGHT = 2;
-        static const int OK_LIMIT = 5.0;
-        static const int NEAR_LIMIT = 3.0;
-        static const int DIST_NAME_NEAR = 0;
-        static const int DIST_NAME_IDEAL = 1;
-        static const int DIST_NAME_FAR = 2;
+
         int left_dist_ = DIST_NAME_FAR;
         int right_dist_ = DIST_NAME_FAR;
         int ahead_dist_ = DIST_NAME_FAR;
         int obstacle_last_seen_ = OBSTACLE_LAST_SEEN_NOWHERE;
-        static const int STATE_SEARCH = 0;
-        static const int STATE_HUGGING = 1;
-        static const int STATE_RECENTLY_LOST = 2;
         int state_;
         int obstacle_direction_ = DIR_UNKNOWN;
 };
