826b7fa5fc6672114b7bbbf597c3830757fea444
Steve Billings <stevebillings@system76-pc.localdomain>
2022-11-21 16:08:01 -0500

    starting work on finding parallel

--- src/obstacle_hugger/src/obstacle_hugger.cpp
+++ src/obstacle_hugger/src/obstacle_hugger.cpp
@@ -8,7 +8,7 @@ static const int STATE_OBSTACLE_NEAR_NOT_PARALLEL = 2;
 static constexpr float SPEED = 0.5;
 
 static const double DIST_WITHIN_SIGHT = 8.0;
-static const double DIST_NEAR = 5.0;
+static const double DIST_NEAR = 3.0;
 static const double SPEED_DIVISOR_FOR_SPIN_YAW = 4.0;
 static const double MAX_SECS_TO_HOPE_FOR_REDISCOVERY = 2.0;
 using std::placeholders::_1;
@@ -33,22 +33,26 @@ class ObstacleHuggingNode : public rclcpp::Node {
 
         void control_callback() {
             RCLCPP_INFO(get_logger(), "======================");
-            RCLCPP_INFO(get_logger(), "State: %d (0 = search; 1 = ahead/far, 2 = near/not-parallel", state_);
+            RCLCPP_INFO(get_logger(), "* State: %d (0 = search; 1 = ahead/far, 2 = near/not-parallel", state_);
             if (last_laser_scan_msg_ == nullptr) {
                 return; // wait for sight
             }
             switch (state_) {
                 case STATE_SEARCH:
                     if (spotted_obstacle(last_laser_scan_msg_) && obstacle_dir(last_laser_scan_msg_) == 0) {
+                        RCLCPP_INFO(get_logger(), "* Obstacle is within sight and straight ahead");
                         if (obstacle_near(last_laser_scan_msg_)) {
+                            RCLCPP_INFO(get_logger(), "* Obstacle is near!");
                             stop();
                             set_state(STATE_OBSTACLE_NEAR_NOT_PARALLEL);
                         } else {
+                            RCLCPP_INFO(get_logger(), "* Obstacle is within sight but not near; drive to it");
                             drive_straight();
                             set_state(STATE_OBSTACLE_AHEAD_FAR);
                         }
                     } else {
-                        spin(-1);
+                        RCLCPP_INFO(get_logger(), "* Obstacle is either out of sight or not yet straight ahead");
+                        spin(-1, calc_spin_velo_mult_search(last_laser_scan_msg_));
                     }
                     break;
                 case STATE_OBSTACLE_AHEAD_FAR:
@@ -60,13 +64,13 @@ class ObstacleHuggingNode : public rclcpp::Node {
                     }
                     break;
                 case STATE_OBSTACLE_NEAR_NOT_PARALLEL:
-                    RCLCPP_INFO(get_logger(), "min_range_index: %ld", get_min_range_index(last_laser_scan_msg_));
+                    RCLCPP_INFO(get_logger(), "* min_range_index: %ld", get_min_range_index(last_laser_scan_msg_));
                     // TODO get parallel
                     int spin_dir = -1;
 //                    if (spotted_obstacle(last_laser_scan_msg_)) {
 //                        spin_dir = obstacle_dir(last_laser_scan_msg_);
 //                    }
-                    spin(spin_dir);
+                    spin(spin_dir, calc_spin_velo_mult_near(last_laser_scan_msg_));
                     break;
             }
         }
@@ -132,41 +136,6 @@ class ObstacleHuggingNode : public rclcpp::Node {
             return min_range_index;
         }
 
-        // TODO working on replacing this
-        /*
-        int get_new_state(sensor_msgs::msg::LaserScan::SharedPtr msg) {
-            unsigned long index_dead_ahead = msg->ranges.size() / 2;
-            unsigned long index_slop = 5;
-
-			int i = 0;
-			auto min_range = std::numeric_limits<double>::max();
-			unsigned long min_range_index = index_dead_ahead;
-			for (auto this_range : msg->ranges) {
-				if (this_range < min_range) {
-					min_range = this_range;
-					min_range_index = i;
-				}
-				i++;
-			}
-            RCLCPP_INFO(get_logger(), "Spotted obstacle at range %lf at index %ld", min_range, min_range_index);
-            int new_state = STATE_SEARCH;
-            if (min_range > 8.0) {
-                new_state = STATE_SEARCH;
-            } else if (min_range_index > (index_dead_ahead + index_slop)) {
-                RCLCPP_INFO(get_logger(), "Obstacle is on the left");
-            } else if (min_range_index < (index_dead_ahead - index_slop)) {
-                RCLCPP_INFO(get_logger(), "Obstacle is on the right");
-            } else {
-                RCLCPP_INFO(get_logger(), "Obstacle is straight ahead");
-                if (min_range < DIST_NEAR) {
-                    new_state = STATE_OBSTACLE_AHEAD_NEAR;
-                } else {
-                    new_state = STATE_OBSTACLE_AHEAD_FAR;
-                }
-            }
-            return new_state;
-		} */
-
         void drive_straight() {
             RCLCPP_INFO(get_logger(), "Driving straight ahead");
             geometry_msgs::msg::Twist drive_message;
@@ -175,8 +144,37 @@ class ObstacleHuggingNode : public rclcpp::Node {
             drive_publisher_->publish(drive_message);
         }
 
-        void spin(int spin_dir) {
-            float yaw = spin_dir * (SPEED / SPEED_DIVISOR_FOR_SPIN_YAW);
+        int calc_spin_velo_mult_search(sensor_msgs::msg::LaserScan::SharedPtr msg) {
+            unsigned long index_dead_ahead = msg->ranges.size() / 2;
+            return calc_spin_velo_mult(msg, index_dead_ahead);
+        }
+
+        int calc_spin_velo_mult_near(sensor_msgs::msg::LaserScan::SharedPtr msg) {
+            unsigned long index_ninety_degrees = 0; // TODO certainly not right
+            return calc_spin_velo_mult(msg, index_ninety_degrees);
+        }
+
+        int calc_spin_velo_mult(sensor_msgs::msg::LaserScan::SharedPtr msg, unsigned long target_dir_index) {
+                // TODO: smooth this out using a single equation
+            unsigned long index_slop = 30;
+            if (!spotted_obstacle(msg)) {
+                return 10;
+            }
+            unsigned long min_range_index = get_min_range_index(msg);
+            if (min_range_index < (target_dir_index - index_slop)) {
+                RCLCPP_INFO(get_logger(), "Obstacle is far right of the target index %ld", target_dir_index);
+                return 2; // in sight on right
+            } else if (min_range_index > (target_dir_index + index_slop)) {
+                RCLCPP_INFO(get_logger(), "Obstacle is far left of the target index %ld", target_dir_index);
+                return 2; // in sight on left
+            } else {
+                RCLCPP_INFO(get_logger(), "Obstacle is close to at the target_dir_index %ld", target_dir_index);
+                return 1; // ahead
+            }
+        }
+
+        void spin(int spin_dir, int spin_velocity_multiplier) {
+            float yaw = spin_velocity_multiplier * spin_dir * (SPEED / SPEED_DIVISOR_FOR_SPIN_YAW);
             geometry_msgs::msg::Twist drive_message;
             drive_message.linear.x = 0.0;
             drive_message.angular.z = yaw; // yaw
